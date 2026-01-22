282112c0a49f103921bf43ccc722ef147b00b5e5
p.pavlov <p.pavlov@yarus.pro>
2026-01-22 20:07:14 +0300

    1

--- postinstall.js
+++ postinstall.js
@@ -1,34 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+
+// Авто-добавление скриптов
 const pkgPath = path.join(process.env.INIT_CWD || process.cwd(), 'package.json');
 
-function checkProjectDeps(deps) {
-    const projectRoot = process.cwd();
-    const missing = [];
-
-    deps.forEach(dep => {
-        const depPath = path.join(projectRoot, 'node_modules', dep);
-        if (!fs.existsSync(depPath)) {
-            missing.push(dep);
-        }
-    });
-    
-    if (missing.length > 0) {
-        console.info(`❌ Установите: npm install ${missing.join(' ')}`);
-        // process.exit(1);
-    }
-}
-
 if (fs.existsSync(pkgPath)) {
-    const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+  
+  if (!pkg.scripts?.build) {
+    pkg.scripts = {
+      ...pkg.scripts,
+      "build": "webpack --env production",
+      "start": "webpack --watch",
+      "yainit":"npx yarus-init",
+    };
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
 
-    if (!pkg.scripts?.build) {
-        pkg.scripts = {
-            ...pkg.scripts,
-            "yainit": "npx yarus-init"
-        };
-        fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
-
-        // checkProjectDeps(['css-loader', 'sass-loader', 'webpack', 'webpack-cli']);
-    }
+    
+  }
 }
